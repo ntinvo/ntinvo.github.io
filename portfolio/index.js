c39b458697bae6c02befa0c12f55f8ea1e1d5252
Tin Vo <tin.vo@ibm.com>
2017-07-07 21:18:31 -0400

    Added a temporary portfolio

---
+++ portfolio/index.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["<i>First</i> sentence.", "Second sentence."],
+  typeSpeed: 40,
+  fadeOut: true,
+  loop: true
+}
+
+var typed = new Typed(".typing_text", options);

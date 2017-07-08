026d69707067f9fe1d43f84a4b1f1d7dadd568b4
Tin Vo <tin.vo@ibm.com>
2017-07-08 10:47:38 -0400

    Deployed

---
+++ index.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["web applications", "user interfaces", "landing pages", "mobile applications"],
+  typeSpeed: 40,
+  fadeOut: true,
+  loop: true
+}
+
+var typed = new Typed(".typing_text", options);
